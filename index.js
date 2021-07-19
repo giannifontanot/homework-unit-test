21fa0c3855f77d65ab2365969a927133112c04e5
giannifontanot <giannifontanot@gmail.com>
2021-07-19 18:40:32 -0500

    The WHILE loop in index.js is working correctly.

--- index.js
+++ index.js
@@ -8,20 +8,13 @@ const fs = require('fs');
 const employee_inquirer = require('./src/employee-inquirer');
 const manager_inquirer = require('./src/manager-inquirer');
 const option_inquirer = require('./src/option_inquirer');
-// const employee_inquirer = require('./src/intern-inquirer');
-// const employee_inquirer = require('./src/engineer-inquirer');
-
-clear();
-
-console.log(
-    chalk.yellow(
-        figlet.textSync('My Team', {
-            horizontalLayout: 'full'
-        })
-    )
-);
+const intern_inquirer = require('./src/intern-inquirer');
+const engineer_inquirer = require('./src/engineer-inquirer');
 
 const run = async () => {
+
+    fClearPage();
+
     // Enter manager's information
     let employeeData = await employee_inquirer.fillData();
     let managerData = await manager_inquirer.fillData();
@@ -29,32 +22,52 @@ const run = async () => {
     let bContinue = true;
     while (bContinue) {
 
+        let employeeData = "";
         let engineerData = "";
         let internData = "";
 
         // What else do you want to do?
+        fClearPage();
         const {option} = await option_inquirer.getOption();
         switch (option) {
             case "add an engineer":
+                fClearPage();
                 employeeData = await employee_inquirer.fillData();
-                managerData = await manager_inquirer.fillData();
+                engineerData = await engineer_inquirer.fillData();
                 break;
             case "add an intern":
+                fClearPage();
                 employeeData = await employee_inquirer.fillData();
-                const managerData = await manager_inquirer.fillData();
+                internData = await intern_inquirer.fillData();
                 break;
             case "finish building my team":
+                fClearPage();
                 bContinue = false;
                 break;
-
         }
-
     }
 
 }
+
 run().then(r => {
-    console.log("r: " + r);
+
 
 });
+
 // console.log(JSON.stringify(optionMenu));
 // // {"option":"add an engineer"}
+
+
+function fClearPage() {
+
+    clear();
+
+    console.log(
+        chalk.yellow(
+            figlet.textSync('My Team', {
+                horizontalLayout: 'full'
+            })
+        )
+    );
+
+}
